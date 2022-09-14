bacba55694a811ca2e16cfb6484ab80948169329
knreddy <knreddy3391@gmail.com>
2022-09-14 18:02:16 +0100

    fixed sonar bug

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -14,12 +14,9 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		// throw new RuntimeException("Some Error has Happened! Contact Support at
-		// ***-***");
 		return new HelloWorldBean("Hello World");
 	}
 
-	/// hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
